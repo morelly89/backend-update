1016d7d34fad27e519f1d66036aa381282e804c2
morelly89 <morelly89@yahoo.com>
2026-08-22 15:21:39 -0400

    Allow production frontend CORS

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
-import express from "express";
 import cors from "cors";
-import routes from "./routes";
-import { errorMiddleware } from "./middleware/error.middleware";
 import dotenv from "dotenv";
+import express from "express";
+import { errorMiddleware } from "./middleware/error.middleware";
 import { authRateLimiter } from "./middleware/rateLimit.middleware";
+import routes from "./routes";
 
 dotenv.config();
 
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://nugget-app-preview.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(authRateLimiter);
